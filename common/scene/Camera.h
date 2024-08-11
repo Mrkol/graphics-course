ba570a0ac6c7913b95992a3f1554fcff276324d5
Roman Sandu <roma.sandu@gmail.com>
2024-08-11 23:24:40 +0300

    More clang-tidy fixes

--- common/scene/Camera.h
+++ common/scene/Camera.h
@@ -18,45 +18,27 @@ struct Camera
     rotation = glm::quatLookAtLH(normalize(to - from), normalize(up));
   }
 
-  void rotate(float upAngleDeg, float rightAngleDeg)
+  void rotate(float up_angle_deg, float right_angle_deg)
   {
-    glm::quat yaw = glm::angleAxis(glm::radians(rightAngleDeg), glm::vec3{0, -1, 0});
-    glm::quat pitch = glm::angleAxis(glm::radians(upAngleDeg), glm::vec3{1, 0, 0});
+    glm::quat yaw = glm::angleAxis(glm::radians(right_angle_deg), glm::vec3{0, -1, 0});
+    glm::quat pitch = glm::angleAxis(glm::radians(up_angle_deg), glm::vec3{1, 0, 0});
     rotation = yaw * rotation * pitch;
   }
 
-  void move(glm::vec3 offset)
-  {
-    position += offset;
-  }
+  void move(glm::vec3 offset) { position += offset; }
 
-  const glm::vec3 right() const
-  {
-    return rotation * glm::vec3{-1, 0, 0};
-  }
+  const glm::vec3 right() const { return rotation * glm::vec3{-1, 0, 0}; }
 
-  const glm::vec3 up() const
-  {
-    return rotation * glm::vec3{0, 1, 0};
-  }
+  const glm::vec3 up() const { return rotation * glm::vec3{0, 1, 0}; }
 
-  const glm::vec3 forward() const
-  {
-    return rotation * glm::vec3{0, 0, 1};
-  }
+  const glm::vec3 forward() const { return rotation * glm::vec3{0, 0, 1}; }
 
   glm::mat4x4 viewItm() const
   {
     return translate(glm::identity<glm::mat4>(), position) * mat4_cast(rotation);
   }
 
-  glm::mat4x4 viewTm() const
-  {
-    return inverse(viewItm());
-  }
+  glm::mat4x4 viewTm() const { return inverse(viewItm()); }
 
-  glm::mat4x4 projTm(float aspect) const
-  {
-    return glm::perspectiveLH_ZO(fov, aspect, zNear, zFar);
-  }
+  glm::mat4x4 projTm(float aspect) const { return glm::perspectiveLH_ZO(fov, aspect, zNear, zFar); }
 };
