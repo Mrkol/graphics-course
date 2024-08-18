8885c04e93a867982a93959f47c06b280ac6b5cf
Roman Sandu <roma.sandu@gmail.com>
2024-08-18 23:09:31 +0300

    Added first 3 tasks
    Added a new directory for tasks and the first 3 tasks. First two are shadertoy exercises, third one is launching the shadertoy shader locally using Vulkan.

---
+++ tasks/local_shadertoy/App.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <etna/Window.hpp>
+#include <etna/PerFrameCmdMgr.hpp>
+#include <etna/ComputePipeline.hpp>
+#include <etna/Image.hpp>
+
+#include "wsi/OsWindowingManager.hpp"
+
+
+class App
+{
+public:
+  App();
+
+  void run();
+
+private:
+  void drawFrame();
+
+private:
+  OsWindowingManager windowing;
+  std::unique_ptr<OsWindow> osWindow;
+
+  glm::uvec2 resolution;
+
+  std::unique_ptr<etna::Window> vkWindow;
+  std::unique_ptr<etna::PerFrameCmdMgr> commandManager;
+};
