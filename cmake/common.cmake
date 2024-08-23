2223f7069e2cb98afc367b40f79b881b27a3200e
Roman Sandu <roma.sandu@gmail.com>
2024-08-24 01:54:04 +0300

    Improve on MSVC
    1) More warnings + treat warnings as errors (except for some insane ones)
    2) Enforce standards compliance
    3) Fix warnings in code

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -4,7 +4,12 @@ set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED True)
 
 if(MSVC)
-  add_compile_options(/W3)
+  add_compile_options(/W4 /WX
+    # Selectively disable some insane warnings
+    /wd4061 /wd4514
+    # Enforce standards-compliance in MSVC
+    /permissive- /volatile:iso /Zc:inline /Zc:wchar_t /EHsc /Zc:preprocessor /Zc:__cplusplus
+  )
 else()
   add_compile_options(-Wall -Wextra -Werror -pedantic)
 endif()
