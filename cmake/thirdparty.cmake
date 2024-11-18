a5d488ee36a36d9e162b4a5c17a1e0e217590f96
Roman Sandu <roma.sandu@gmail.com>
2024-11-18 23:11:29 +0300

    Bump etna to 1.7.0

--- cmake/thirdparty.cmake
+++ cmake/thirdparty.cmake
@@ -54,7 +54,7 @@ CPMAddPackage(
 CPMAddPackage(
   NAME etna
   GITHUB_REPOSITORY AlexandrShcherbakov/etna
-  VERSION 1.6.2
+  VERSION 1.7.0
 )
 
 # Type-erased function containers that actually work
