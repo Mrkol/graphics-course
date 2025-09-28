33636d1ba107cd6ab3427654bb38ca6d7c08f8b5
Karetskii Vlad <kar.vladi@yandex.ru>
2025-09-28 17:01:24 +0300

    hw done

--- tasks/local_shadertoy1/App.hpp
+++ tasks/local_shadertoy1/App.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <chrono>
+
 #include <etna/Window.hpp>
 #include <etna/PerFrameCmdMgr.hpp>
 #include <etna/ComputePipeline.hpp>
 #include <etna/Image.hpp>
+#include <etna/Sampler.hpp>
 
 #include "wsi/OsWindowingManager.hpp"
 
@@ -11,21 +14,35 @@
 class App
 {
 public:
-  App();
-  ~App();
+    App();
+    ~App();
 
-  void run();
-
-private:
-  void drawFrame();
+    void run();
 
 private:
-  OsWindowingManager windowing;
-  std::unique_ptr<OsWindow> osWindow;
+    void drawFrame();
+    
+    void update();
+private:
+    OsWindowingManager windowing;
+    std::unique_ptr<OsWindow> osWindow;
 
-  glm::uvec2 resolution;
-  bool useVsync;
+    glm::uvec2 resolution;
+    bool useVsync;
 
-  std::unique_ptr<etna::Window> vkWindow;
-  std::unique_ptr<etna::PerFrameCmdMgr> commandManager;
+    std::unique_ptr<etna::Window> vkWindow;
+    std::unique_ptr<etna::PerFrameCmdMgr> commandManager;
+
+    etna::Sampler sampler;
+    etna::Image result;
+    etna::ComputePipeline pipeline;
+
+    struct PushConstants {
+        uint32_t resolutionX, resolutionY;
+        float mouseX, mouseY;
+        float time;
+    };
+
+    PushConstants pushConstants;
+    std::chrono::steady_clock::time_point start;
 };
